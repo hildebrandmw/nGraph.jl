edc0cf38bb32e09a5680a404efe70eeb3f8e58cb
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-05-23 01:09:53 +0000

    Now with 100% less segfaults

--- src/compile.jl
+++ src/compile.jl
@@ -13,25 +13,19 @@ mutable struct Executable
     ptr::Lib.CxxWrap.SmartPointerWithDeref{nGraph.Lib.Executable,:St10shared_ptrIiE}
     ngraph_function::NFunction
     backend::Backend
-    iscleared::Bool
 
     function Executable(ptr, ngraph_function::NFunction, backend::Backend)
-        ex = new(ptr, ngraph_function, backend, false)
-        finalizer(_cleanup, ex)
+        ex = new(ptr, ngraph_function, backend)
+
+        # Immediately clear this from the saved functions
+        #
+        # This avoids needing to clean it up later.
+        Lib.remove_compiled_function(getpointer(ex.backend), getpointer(ex))
+
         return ex
     end
 end
 wraptype(::Executable) = HasPointer()
-
-# Free resources held by the executable
-function _cleanup(ex::Executable)
-    # Don't try to remove an executable more than once if it was cleaned up elsewhere
-    if ex.iscleared == false
-        ex.iscleared = true
-        Lib.remove_compiled_function(getpointer(ex.backend), getpointer(ex))
-    end
-end
-
 
 function compile(backend::Backend, inputs::ParameterVector, outputs::NodeVector)
     ngraph_function = NFunction(outputs, inputs)
@@ -43,20 +37,16 @@ function compile(backend::Backend, inputs::ParameterVector, outputs::NodeVector)
 end
 
 function (ex::Executable)(inputs::Vector{Any}, outputs::Vector{Any}) 
-    ex.iscleared && error("Executable is cleared")
     Lib.call(getpointer(ex), outputs, inputs)
 end
 
+"""
+    recompile(ex::Executable)
+
+WARNING: Don't call ANY previous executables after calling this function.
+"""
 function recompile(ex::Executable)
     backend = ex.backend
-
-    # Make sure that this executable is not called again, because that would be
-    # very wrong. The executable should go out of scope and be cleared up by the 
-    # finalizer.
-    _cleanup(ex)
-
-    # If this executable was holding onto some large buffers, call the GC now.
-    GC.gc()
 
     # Assume we're working in the same directory as the "cpu_codegen" directory.
     #
