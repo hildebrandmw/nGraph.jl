1ef24635f83372ac298afcc6d25469818c713be3
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-09-24 00:17:05 +0000

    Some bug tracking support

--- src/special.jl
+++
@@ -1,8 +0,0 @@
-# Wrappers for parameters that require special handling.
-#
-# The main culprate are embedding tables, that can efficiently use an in-place update.
-struct EmbeddingWeights{T}
-    x::T
-end
-
-
